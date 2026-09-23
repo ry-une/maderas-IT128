ed01b38854f79d186814c1932a6195b0695362c4
Ryan Joseph V. Maderas <ryanmaderas@proton.me>
2026-09-22 20:17:06 -0400

    Complete BlogDataLibrary models and data access

---
+++ MaderasLE2/BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,14 @@
+﻿using BlogDataLibrary.Database;
+
+namespace BlogDataLibrary.Data;
+
+public class SqlData
+{
+    private readonly ISqlDataAccess _db;
+    private const string connectionStringName = "SqlDb";
+
+    public SqlData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+}
---
+++ MaderasLE2/BlogDataLibrary/Database/SqlDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogDataLibrary.Database;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public List<T> LoadData<T, U>(
+        string sqlStatement, U parameters,
+        string connectionStringName, bool isStoredProcedure)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+        CommandType commandType = isStoredProcedure
+            ? CommandType.StoredProcedure
+            : CommandType.Text;
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+        return connection.Query<T>(
+            sqlStatement, parameters, commandType: commandType).ToList();
+    }
+
+    public void SaveData<T>(
+        string sqlStatement, T parameters,
+        string connectionStringName, bool isStoredProcedure)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+        CommandType commandType = isStoredProcedure
+            ? CommandType.StoredProcedure
+            : CommandType.Text;
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+        connection.Execute(
+            sqlStatement, parameters, commandType: commandType);
+    }
+}
