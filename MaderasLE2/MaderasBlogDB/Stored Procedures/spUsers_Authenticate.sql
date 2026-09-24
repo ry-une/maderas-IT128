867f9a4110910fe8041102ad15838fe6abacef94
Ryan Joseph V. Maderas <ryanmaderas@proton.me>
2026-09-24 15:05:52 -0400

    Complete Lab Exercise 3

---
+++ MaderasLE2/MaderasBlogDB/Stored Procedures/spUsers_Authenticate.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Authenticate]
+    @username nvarchar(16),
+    @password nvarchar(16)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT [Id], [UserName], [FirstName], [LastName], [Password]
+    FROM dbo.Users
+    WHERE UserName = @username
+      AND Password = @password;
+END
---
+++ MaderasLE2/MaderasBlogDB/spPosts_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+    @userId int,
+    @title nvarchar(150),
+    @body text,
+    @dateCreated datetime2
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.Posts
+        (UserId, Title, Body, DateCreated)
+    VALUES
+        (@userId, @title, @body, @dateCreated);
+END
